1bbb99daccf336a371e7202100ab496ad79307b0
Mathis Barré <mathis.barre@live.fr>
2021-07-15 11:54:46 +0200

    feat: minimal functionnal example

--- typeDefs.ts
+++ typeDefs.ts
@@ -2,38 +2,9 @@ import { gql } from 'apollo-server-fastify';
 import { DocumentNode } from 'graphql';
 
 const typeDefs: DocumentNode = gql`
-  type Album {
-    name: String!
-  }
-
-  type Artist {
-    name: String!
-  }
-
-  enum TrackType {
-    track # this is the only type in sample data for now
-  }
-
-  type Tracks {
-    added_at: String!,
-    track: {
-      album: Album
-      artists: [Artist!]!,
-      name: String!,
-      preview_url: String!,
-      type: TrackType
-    }
-  }
-
-  type Owner {
-    display_name: String!
-  }
-
-  type Playlist: {
+  type Playlist {
     name: String!,
     description: String!,
-    owner: Owner,
-    tracks: [Tracks!]!
   }
 
   type Query {
